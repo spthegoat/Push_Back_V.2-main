fc00bef75b50d44776c16bdf55b0ead68bd4be15
Sahil Patel <srpthegoat@gmail.com>
2025-12-09 10:09:59 -0500

    n

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -8,7 +8,7 @@ extern Drive chassis;
 // Your motors, sensors, etc. should go here.  Below are examples
 
 inline pros::Motor front_intake(21);
-inline pros::Motor back_intake(20);
+inline pros::Motor middle_intake(20);
 inline pros::Motor top_intake(12);
 
 inline ez::Piston piston1('H', true);
--- src/main.cpp
+++ src/main.cpp
@@ -121,20 +121,17 @@ void autonomous() {
   chassis.drive_imu_reset();                  // Reset gyro position to 0
   chassis.drive_sensor_reset();               // Reset drive sensors to 0
   chassis.odom_xyt_set(0_in, 0_in, 0_deg);    // Set the current position, you can start at a specific position with this
-  chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistency
+  chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistenry and Pure Pursuit are not magic
 
-  /*
-  Odometry and Pure Pursuit are not magic
+  //It is possible to get perfectly consistent results without tracking wheels,
+  // but it is also possible to have extremely inconsistent results without tracking wheels.
+ //  When you don't use tracking wheels, you need to:
+  // - avoid wheel slip
+ //  - avoid wheelies
+//   - avoid throwing momentum around (super harsh turns, like in the example below)
+ // You can do cool curved motions, but you have to give your robot the best chance
+ // to be consistent
 
-  It is possible to get perfectly consistent results without tracking wheels,
-  but it is also possible to have extremely inconsistent results without tracking wheels.
-  When you don't use tracking wheels, you need to:
-   - avoid wheel slip
-   - avoid wheelies
-   - avoid throwing momentum around (super harsh turns, like in the example below)
-  You can do cool curved motions, but you have to give your robot the best chance
-  to be consistent
-  */
 
   ez::as::auton_selector.selected_auton_call();  // Calls selected auton from autonomous selector
 }
@@ -156,7 +153,7 @@ void screen_print_tracker(ez::tracking_wheel *tracker, std::string name, int lin
  * Ez screen task
  * Adding new pages here will let you view them during user control or autonomous
  * and will help you debug problems you're having
- */
+ 
 void ez_screen_task() {
   while (true) {
     // Only run this when not connected to a competition switch
@@ -263,20 +260,20 @@ void opcontrol() {
     // middle goal
     piston1.set(false); 
     front_intake.move(115);
-    back_intake.move(115);
+    middle_intake.move(115);
     top_intake.move(-115);
   } else if (master.get_digital(DIGITAL_L2)) {
     // outtake
     piston1.set(false);
     front_intake.move(-80);
-    back_intake.move(80);
+    middle_intake.move(80);
     top_intake.move(0);
  // Retract piston for outtake
   } else if (master.get_digital(DIGITAL_R1)) {
        // High goal
     piston1.set(true);
     front_intake.move(127);
-    back_intake.move(127);
+    middle_intake.move(127);
     top_intake.move(127);
  // Extend piston for high goal
   } else if (master.get_digital(DIGITAL_RIGHT)) {
@@ -287,20 +284,20 @@ void opcontrol() {
     // Basket
     piston1.set(false);
     front_intake.move(127);
-    back_intake.move(0);
+    middle_intake.move(0);
     top_intake.move(127);
  // Retract piston 
     } else if (master.get_digital(DIGITAL_DOWN)) {
     // High goal
     piston1.set(false); 
     front_intake.move(127);
-    back_intake.move(-127);
+    middle_intake.move(-127);
     top_intake.move(-127);
     } else {
     // Stop intake
     piston1.set(false);
     front_intake.move(0);
-    back_intake.move(0);
+    middle_intake.move(0);
     top_intake.move(0);
    // Retract piston when stopped
   }
